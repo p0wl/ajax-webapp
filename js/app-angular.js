1668d6764f31593fc2d77c3982db5af4157051e2
Paul Mölders <paulmoelders@gmail.com>
2013-01-13 16:46:51 +0100

    initial angular setup
    counter works, add not yet

---
+++ js/app-angular.js
@@ -0,0 +1,11 @@
+// App-Angular.js
+
+function VoteCtrl($scope, $http) {
+	// Data Storage
+	$scope.frameworks = [
+		{text: 'jQuery', votes: '3'},
+		{text: 'Zepto', votes: '1'},
+		{text: 'Underscore', votes: '7'}
+	];
+}
+
