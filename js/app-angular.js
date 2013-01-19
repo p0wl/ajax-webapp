4dbd34369e4fc771d109ecc9807dc3195a5c75eb
Paul Mölders <paulmoelders@gmail.com>
2013-01-19 13:12:47 +0100

    added "add choice" functionality and filtering

--- js/app-angular.js
+++ js/app-angular.js
@@ -2,10 +2,24 @@
 
 function VoteCtrl($scope, $http) {
 	// Data Storage
+	// TODO: REST
 	$scope.frameworks = [
 		{text: 'jQuery', votes: '3'},
 		{text: 'Zepto', votes: '1'},
 		{text: 'Underscore', votes: '7'}
 	];
+
+	// Add Choice Function
+	$scope.addChoice = function () {
+		$scope.frameworks.push({text: $scope.choiceName, votes: '1'});
+		// TODO: REST
+
+		// Input zurücksetzen
+		$scope.choiceName = '';
+		// Layout zurücksetzen
+		$scope.newchoice = false;
+		// Filter zurücksetzen
+		$scope.search = '';
+	};
 }
 
